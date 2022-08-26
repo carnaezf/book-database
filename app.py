b639b1717ef49966b992daee76ed456909a70468
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2022-08-25 21:04:34 -0400

    able to view a book

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def clean_date(date_str):
         year = int(split_date[2])
         return_date = datetime.date(year, month, day)
     except ValueError:
-        print("""
+        input("""
               \n*******DATE ERROR*******
               \rThe date format should include a valid Month Day, Year from the past.
               \rEx: January 13, 2003
@@ -44,13 +44,11 @@ def clean_date(date_str):
     else:
         return return_date
 
-    
-
 def clean_price(price_str):
     try:
         price_float = float(price_str)
     except ValueError:
-        print("""
+        input("""
               \n*******PRICE ERROR*******
               \rThe price should be a number without a currency symbol.
               \rEx: 10.99
@@ -58,6 +56,29 @@ def clean_price(price_str):
               \r*************************""")
     else:
          return int(price_float * 100)
+
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+        
+    except ValueError:
+        input("""
+              \n*******ID ERROR*******
+              \The id should be a number.
+              \rPress Enter to try again
+              \r*************************""")
+        return 
+    else:
+        if book_id in options:
+             return book_id
+        else:
+            input(f"""
+              \n*******ID ERROR*******
+              \rOptions: {options}
+              \rPress Enter to try again
+              \r*************************""")
+            return
+
 
 def add_csv():
     with open('suggested_books.csv') as csvfile:
@@ -72,7 +93,6 @@ def add_csv():
                 new_book = Book(title=title, author=author, published_date=date, price=price)
                 session.add(new_book)
         session.commit()
-            
 
 def app():
     app_running = True
@@ -99,31 +119,48 @@ def app():
             session.commit()
             print('Book added!')
             time.sleep(1.5)
-            
+
         elif choice == '2':
             # View all books
             for book in session.query(Book):
                 print(f'{book.id}   |   {book.title}   |   {book.author}')
             input('\nPress Enter to tha main menu')
+
         elif choice == '3':
-            # Seach for book
-            pass
+            # Seach book
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                    \nId Options: {id_options}
+                    \rBook id:  ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+                  \n{the_book.title} by {the_book.author}
+                  \rPublished: {the_book.published_date}
+                  \rPrice ${the_book.price / 100}''')
+            input('\rPress enter to return the main Menu')
+
+
         elif choice =='4':
             # Book Analysis
             pass
         else:
             print('GOODBYE')
             app_running = False
-        
-
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     add_csv()
     app()
     
-    for book in session.query(Book):
-        print(book)
+    # for book in session.query(Book):
+    #     print(book)
 
 
 
